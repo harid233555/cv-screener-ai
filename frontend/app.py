e5abca9176079c3bf9b8c3ca7f96770a8b64c8b0
harid233555 <harid8149@gmail.com>
2025-06-19 22:36:36 +0530

    Create app.py

---
+++ frontend/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import requests
+
+st.title("AI CV Screener")
+jd_text = st.text_area("Paste Job Description")
+uploaded_file = st.file_uploader("Upload CV (PDF)")
+
+if uploaded_file and jd_text:
+    files = {'file': uploaded_file}
+    data = {'job_description': jd_text}
+    res = requests.post("https://cv-api-nikita.streamlit.app/screen", files=files, data=data)
+    if res.ok:
+        output = res.json()
+        st.success(f"Candidate: {output['candidate']}")
+        st.info(f"Score: {output['score']}")
+        st.info(f"Similarity: {output['similarity']:.2f}")
+    else:
+        st.error("Error occurred while processing the CV.")
